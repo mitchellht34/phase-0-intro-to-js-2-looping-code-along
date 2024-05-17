b55c1a50e7c7da07da5bab9bb17fb17bf10282f2
Mitch Toran <mitchellht34@gmail.com>
2024-05-17 14:00:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for(let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        //debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function writeCards(names, event) {
+    const messages = [];
+    for(let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+
+    return messages;
+}
+
+function countDown(int) {
+    while(int >= 0){
+        console.log(int);
+        int--;
+    }
+}
